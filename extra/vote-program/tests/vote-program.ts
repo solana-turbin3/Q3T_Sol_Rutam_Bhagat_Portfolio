f69b032bdddaf5371de41f9442620796b7de39e8
RutamBhagat <rutambhagat@gmail.com>
2024-08-06 03:37:57 +0530

    test(vote): add tests for program instructions

--- extra/vote-program/tests/vote-program.ts
+++ extra/vote-program/tests/vote-program.ts
@@ -1,16 +1,62 @@
 import * as anchor from "@coral-xyz/anchor";
+
 import { Program } from "@coral-xyz/anchor";
 import { VoteProgram } from "../target/types/vote_program";
 
 describe("vote-program", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.VoteProgram as Program<VoteProgram>;
 
+  const url = "https://wba.dev";
+
+  const [voteAccount] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(url)],
+    program.programId
+  );
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .initialize(url)
+      .accounts({
+        payer: provider.wallet.publicKey,
+      })
+      .rpc();
+
     console.log("Your transaction signature", tx);
+
+    let voteState = await program.account.voteState.fetch(voteAccount);
+    console.log("Your vote score is", voteState.score.toString());
+  });
+
+  it("Upvote!", async () => {
+    const tx = await program.methods
+      .upvote(url)
+      .accounts({
+        voteAccount,
+      })
+      .rpc();
+
+    console.log("Your transaction signature", tx);
+
+    let voteState = await program.account.voteState.fetch(voteAccount);
+    console.log("Your vote score is", voteState.score.toString());
+  });
+
+  it("Downvote!", async () => {
+    const tx = await program.methods
+      .downvote(url)
+      .accounts({
+        voteAccount,
+      })
+      .rpc();
+
+    console.log("Your transaction signature", tx);
+
+    let voteState = await program.account.voteState.fetch(voteAccount);
+    console.log("Your vote score is", voteState.score.toString());
   });
 });
